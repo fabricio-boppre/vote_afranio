59c5d71bab7797c0b7afba0c9eb98ced60e2d4bc
Fabricio Chiquio Boppré <fabricio.boppre@gmail.com>
2024-08-02 17:19:58 -0300

    Packages updated.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from "astro/config"
 
-import pageInsight from "astro-page-insight";
+import pageInsight from "astro-page-insight"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://vote.afranio.org.br",
-  integrations: [pageInsight()]
-});
+  integrations: [pageInsight()],
+})
