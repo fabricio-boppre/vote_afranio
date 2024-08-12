497a62f271df18816092b9cd91730d82c0b49587
Fabricio Chiquio Boppré <fabricio.boppre@gmail.com>
2024-08-12 10:03:05 -0300

    Packages updated and Astro SEO configuration finished.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from "astro/config"
+import { defineConfig } from "astro/config";
+import pageInsight from "astro-page-insight";
 
-import pageInsight from "astro-page-insight"
+import metaTags from "astro-meta-tags";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://vote.afranio.org.br",
-  integrations: [pageInsight()],
-})
+  integrations: [pageInsight(), metaTags()]
+});
